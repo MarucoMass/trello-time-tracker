8d494d071afe62880679a0c652bf993554adc57b
MarucoMass <mariomass23@gmail.com>
2024-06-11 00:59:43 -0300

    (Header): decrease p-y, (Aside): changed fsize and styles

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,12 +1,20 @@
+import { Avatar } from "./ui/avatar";
 import { Separator } from "./ui/separator";
 
 const Aside = () => {
-  const name = "hola guachin";
+  const name = "Maruco";
   return (
     <aside id="aside" className="w-1/4 h-dvh bg-muted">
-      <div className="p-4 flex flex-col">
-        <h2 className="text-xl font-bold">{name}</h2>
-        <span className="text-xs italic">hello</span>
+      <div id="user" className="flex items-center p-4 gap-2">
+        <Avatar>
+          <div className="w-full h-full bg-foreground text-background text-lg font-semibold flex items-center justify-center">
+            {name[0].toUpperCase()}
+          </div>
+        </Avatar>
+        <div className=" flex flex-col">
+          <h2 className="text-sm font-bold">{name}</h2>
+          <span className="text-xs italic">Gratuito</span>
+        </div>
       </div>
 
       <Separator />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import {Table2} from "lucide-react"
 
 const Header = () => {
   return (
-    <header className="w-full h-16 bg-foreground text-background flex items-center py-6 px-4 gap-1 shadow-lg">
+    <header className="w-full h-12 bg-foreground text-background flex items-center p-4 gap-1 shadow-lg">
         <Table2 size={24}/>
-        <h1 className="font-medium text-2xl">Trello</h1>
+        <h1 className="font-medium text-xl">Trello</h1>
     </header>
   )
 }
